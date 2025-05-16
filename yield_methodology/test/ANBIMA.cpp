e58e5b28db80d67a05065cfa0a71fdf840754165
Andrey Gorbachev <andreygorbachev@hotmail.com>
2025-05-16 14:37:12 +0100

    Add tests for LFT.

--- yield_methodology/test/ANBIMA.cpp
+++ yield_methodology/test/ANBIMA.cpp
@@ -43,7 +43,7 @@ using namespace reset;
 namespace debt_security
 {
 
-	TEST(ANBIMA, price1)
+	TEST(ANBIMA, LTN1)
 	{
 		// from "Methodology for Calculating Federal Government Bonds Offered in Primary Auctions"
 
@@ -71,15 +71,14 @@ namespace debt_security
 		EXPECT_EQ(price, 753.315323);
 	}
 
-
-	TEST(ANBIMA, price2)
+	TEST(ANBIMA, LTN2)
 	{
 		// from "Methodology for Calculating Federal Government Bonds Offered in Primary Auctions"
 
 		const auto issue_date = 2007y / July / 1d; // made up (does not matter)
 		const auto maturity_date = 2010y / July / 1d;
 		const auto& calendar = make_calendar_ANBIMA();
-		const auto face = cpp_dec_float_50{ "1000" };
+		const auto face = cpp_dec_float_50{ 1000 };
 		const auto LTN = debt_security::bill{ issue_date, maturity_date, calendar, face };
 
 		const auto settlement_date = 2008y / May / 21d;
@@ -98,6 +97,63 @@ namespace debt_security
 		const auto yield = cpp_dec_float_50{ "0.1436" };
 		const auto price = ANBIMA.price(yield, LTN, quote);
 		EXPECT_EQ(price, cpp_dec_float_50{ "753.315323" });
+	}
+
+
+	TEST(ANBIMA, LFT1)
+	{
+		// from "Methodology for Calculating Federal Government Bonds Offered in Primary Auctions"
+
+		const auto issue_date = 2000y / July / 1d; // made up (does not matter)
+		const auto maturity_date = 2014y / March / 7d;
+		const auto& calendar = make_calendar_ANBIMA();
+		const auto face = 100.0;
+		const auto LFT = debt_security::bill{ issue_date, maturity_date, calendar, face };
+
+		const auto settlement_date = 2008y / May / 21d;
+		const auto truncate = 4u;
+		const auto quote = debt_security::quote{ settlement_date, face, truncate };
+
+		const auto ANBIMA = debt_security::ANBIMA{};
+
+		// check the setup of the test first
+		const auto bd = calendar.count_business_days(days_period{
+			settlement_date,
+			sys_days{ maturity_date } - days{ 1 } // "end date" should be excluded
+		});
+		EXPECT_EQ(bd, 1459/*zu*/);
+
+		const auto yield = -0.0002;
+		const auto price = ANBIMA.price(yield, LFT, quote);
+		EXPECT_EQ(price, 100.1158);
+	}
+
+	TEST(ANBIMA, LFT2)
+	{
+		// from "Methodology for Calculating Federal Government Bonds Offered in Primary Auctions"
+
+		const auto issue_date = 2000y / July / 1d; // made up (does not matter)
+		const auto maturity_date = 2014y / March / 7d;
+		const auto& calendar = make_calendar_ANBIMA();
+		const auto face = cpp_dec_float_50{ 100 };
+		const auto LFT = debt_security::bill{ issue_date, maturity_date, calendar, face };
+
+		const auto settlement_date = 2008y / May / 21d;
+		const auto truncate = 4u;
+		const auto quote = debt_security::quote{ settlement_date, face, truncate };
+
+		const auto ANBIMA = debt_security::ANBIMA<cpp_dec_float_50>{};
+
+		// check the setup of the test first
+		const auto bd = calendar.count_business_days(days_period{
+			settlement_date,
+			sys_days{ maturity_date } - days{ 1 } // "end date" should be excluded
+		});
+		EXPECT_EQ(bd, 1459/*zu*/);
+
+		const auto yield = cpp_dec_float_50{ "-0.0002" };
+		const auto price = ANBIMA.price(yield, LFT, quote);
+		EXPECT_EQ(price, cpp_dec_float_50{ "100.1158" });
 	}
 
 }
