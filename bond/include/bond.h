9c0e96df8ed9ca85fb8f71892c2af8c58a4a7f19
Andrey Gorbachev <andreygorbachev@hotmail.com>
2025-05-16 16:03:56 +0100

    Started thinking about bonds.

---
+++ bond/include/bond.h
@@ -0,0 +1,120 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2025 Andrey Gorbachev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include <chrono>
+#include <utility>
+
+#include <calendar.h>
+
+#include <following.h>
+#include <cash_flow.h>
+
+
+namespace debt_security
+{
+
+	template<typename T = double>
+	class bond // how bill and bond are related? (is bill a bond?) 
+	{
+
+	public:
+
+		explicit bond(
+			std::chrono::year_month_day issue_date,
+			std::chrono::year_month_day maturity_date, // or should these 2 be captured as a georgian::period?
+			gregorian::calendar cal, // do we want to copy these everywhere?
+			T face = 100 // do we care for this? (or is it just part of price?) if we do not have it we'll have to have cashflow to be based on a unit notional
+		) noexcept;
+
+	public:
+
+		auto get_issue_date() const noexcept -> const std::chrono::year_month_day&;
+		auto get_maturity_date() const noexcept -> const std::chrono::year_month_day&;
+		auto get_calendar() const noexcept -> const gregorian::calendar&;
+		auto get_face() const noexcept -> const T&;
+
+	public:
+
+		auto cash_flow() const -> fin_calendar::cash_flow<T>; // should we also return a cashflow at the issuance going the other way? (for that we'll need to capture issue price somehow)
+
+	private:
+
+		std::chrono::year_month_day issue_date_{};
+		std::chrono::year_month_day maturity_date_{};
+		gregorian::calendar cal_{};
+		T face_{};
+
+	};
+
+
+	template<typename T>
+	bond<T>::bond(
+		std::chrono::year_month_day issue_date,
+		std::chrono::year_month_day maturity_date,
+		gregorian::calendar cal,
+		T face
+	) noexcept :
+		issue_date_{ std::move(issue_date) },
+		maturity_date_{ std::move(maturity_date) },
+		cal_{ std::move(cal) },
+		face_{ std::move(face) }
+	{
+	}
+
+
+	template<typename T>
+	auto bond<T>::get_issue_date() const noexcept -> const std::chrono::year_month_day&
+	{
+		return issue_date_;
+	}
+
+	template<typename T>
+	auto bond<T>::get_maturity_date() const noexcept -> const std::chrono::year_month_day&
+	{
+		return maturity_date_;
+	}
+
+	template<typename T>
+	auto bond<T>::get_calendar() const noexcept -> const gregorian::calendar&
+	{
+		return cal_;
+	}
+
+	template<typename T>
+	auto bond<T>::get_face() const noexcept -> const T&
+	{
+		return face_;
+	}
+
+
+	template<typename T>
+	auto bond<T>::cash_flow() const -> fin_calendar::cash_flow<T> // do we want to cache this? (and return a const reference?)
+	{
+		const auto f = fin_calendar::following{};
+		const auto payment_date = f.adjust(maturity_date_, cal_);
+
+		return fin_calendar::cash_flow<T>{ payment_date, face_ };
+	}
+
+}
---
+++ bond/test/bond.cpp
@@ -0,0 +1,64 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2025 Andrey Gorbachev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "setup.h"
+
+#include <bond.h>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+using namespace std::chrono;
+
+
+namespace debt_security
+{
+
+	TEST(bond, constructor1)
+	{
+		const auto issue_date = 2025y / January / 1d;
+		const auto maturity_date = 2025y / February / 1d;
+		const auto& calendar = make_calendar_ANBIMA();
+		const auto face = 1000.0;
+		const auto b = bond{ issue_date, maturity_date, calendar, face };
+
+		EXPECT_EQ(b.get_issue_date(), issue_date);
+		EXPECT_EQ(b.get_maturity_date(), maturity_date);
+		EXPECT_EQ(b.get_calendar(), calendar);
+		EXPECT_EQ(b.get_face(), face);
+	}
+
+	TEST(bond, cash_flow1)
+	{
+		const auto issue_date = 2025y / January / 1d;
+		const auto maturity_date = 2025y / February / 1d;
+		const auto& calendar = make_calendar_ANBIMA();
+		const auto face = 1000.0;
+		const auto b = bond{ issue_date, maturity_date, calendar, face };
+
+		const auto cf = b.cash_flow();
+
+		EXPECT_EQ(cf.get_payment_date(), 2025y / February / 3d);
+		EXPECT_EQ(cf.get_amount(), face);
+	}
+
+}
